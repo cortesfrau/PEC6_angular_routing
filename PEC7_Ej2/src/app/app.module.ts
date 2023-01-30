e266b7cb483e17c46a3fc7eee316b8ca61c1e597
Lluis Cortes Frau <lluiscortes@uoc.edu>
2023-01-30 13:44:04 +0100

    Exercise 2

--- PEC7_Ej2/src/app/app.module.ts
+++ PEC7_Ej2/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { AppComponent } from './app.component';
 import { WineitemComponent } from './wines/wineitem/wineitem.component';
 import { WinelistComponent } from './wines/winelist/winelist.component';
 import { WineNewComponent } from './wines/winenew/winenew.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { WineService } from './services/wine.service';
 import { ImageWinePipe } from './pipes/image-wine.pipe';
+import { RouterModule } from "@angular/router";
+import { LoginComponent } from "./users/login/login.component";
+import { RegisterComponent } from "./users/register/register.component";
+import { WineDetailsComponent } from "./wines/winedetails/winedetails.component";
+import { UserService } from "./services/user.service";
+import { UserStoreService } from "./services/userstore.service";
+import { WineAppInterceptor } from "./services/wineapp.interceptor";
+
 
 @NgModule({
   declarations: [
@@ -15,15 +23,27 @@ import { ImageWinePipe } from './pipes/image-wine.pipe';
     WineitemComponent,
     WinelistComponent,
     WineNewComponent,
-    ImageWinePipe
+    ImageWinePipe,
+    WineDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([]),
   ],
-  providers: [WineService],
+  providers: [
+    WineService,
+    UserStoreService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: WineAppInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ PEC7_Ej2/src/app/services/userstore.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class UserStoreService {
+  private _token: string = null;
+  constructor() {
+    this._token = localStorage.getItem("wine-token") || null;
+  }
+
+  set token(token: string) {
+    this._token = token;
+    localStorage.setItem("wine-token", token);
+  }
+
+  get token() {
+    return this._token;
+  }
+
+  isLoggedIn() {
+    return this.token !== null;
+  }
+}
